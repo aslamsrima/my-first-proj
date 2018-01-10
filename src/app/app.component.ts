f3d52f8fd430b25b8ce79672a81a41a8b56a6fcb
ETERNUS\aslam.shrimali <aslam.shrimali@eternussolutions.com>
2018-01-10 10:15:38 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { Hero} from './heroes/hero';
+import { DataService } from './data.service';
+import { Http } from '@angular/http';
+import { trigger,state,style,transition,animate,keyframes} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl:'./app.component.html',
+  styleUrls: ['./app.component.css'],
+  
+
 })
 export class AppComponent {
-  title = 'app';
+
+  myData=[];
+  
+  constructor(private http:Http){
+    this.http.get('https://jsonplaceholder.typicode.com/photos')
+      .subscribe(res =>{
+       this.myData=res.json();
+       console.log(this.myData);
+      });
+      
+     };
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule} from '@angular/http';
 
 import { AppComponent } from './app.component';
-
-
+import { HeroesComponent } from './heroes/heroes.component';
+import { DataService } from './data.service';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatButtonModule,MatCardModule,MatMenuModule,MatToolbarModule,MatIconModule} from '@angular/material';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule
+
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
